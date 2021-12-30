f0f013e6dc7fce7e129e64eeb888ea0d8d6a5192
Banana Man <banana@bananaboogie.com>
2021-12-30 17:56:28 -0600

    Fluid desktop nav complete!

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -61,8 +61,8 @@ const MainHeader = styled.div`
   display: flex;
   align-items: baseline;
   padding: 18px 32px;
-  height: 72px;
   border-bottom: 1px solid ${COLORS.gray[300]};
+  overflow: auto;
 
   @media (${QUERIES.tabletAndSmaller}) {
     border-top: 4px ${COLORS.gray[900]} solid;
@@ -79,7 +79,7 @@ const MainHeader = styled.div`
 
 const DesktopNav = styled.nav`
   display: flex;
-  gap: 48px;
+  gap: clamp(1rem, 7.7vw - 3.75rem, 3rem);
   margin: 0px 48px;
 
   @media (${QUERIES.tabletAndSmaller}) {
