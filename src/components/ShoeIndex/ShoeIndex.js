a450d4d6009c42942366327d97f4aa78ae4ee71a
Banana Man <banana@bananaboogie.com>
2021-12-30 16:49:03 -0600

    Main view complete

--- src/components/ShoeIndex/ShoeIndex.js
+++ src/components/ShoeIndex/ShoeIndex.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components/macro';
 
-import { WEIGHTS } from '../../constants';
+import { QUERIES, WEIGHTS } from '../../constants';
 
 import Breadcrumbs from '../Breadcrumbs';
 import Select from '../Select';
@@ -14,33 +14,64 @@ const ShoeIndex = ({ sortId, setSortId }) => {
     <Wrapper>
       <MainColumn>
         <Header>
-          <Title>Running</Title>
-          <Select
-            label="Sort"
-            value={sortId}
-            onChange={(ev) => setSortId(ev.target.value)}
-          >
-            <option value="newest">Newest Releases</option>
-            <option value="price">Price</option>
-          </Select>
+          <TitleWrapper>
+            <BreadcrumbsMobileWrapper>
+              <BreadCrumbsDisplay />
+            </BreadcrumbsMobileWrapper>
+            <Title>Running</Title>
+          </TitleWrapper>
+          <SelectWrapper>
+            <Select
+              label="Sort"
+              value={sortId}
+              onChange={(ev) => setSortId(ev.target.value)}
+            >
+              <option value="newest">Newest Releases</option>
+              <option value="price">Price</option>
+            </Select>
+          </SelectWrapper>
         </Header>
         <Spacer size={32} />
         <ShoeGrid />
       </MainColumn>
       <LeftColumn>
-        <Breadcrumbs>
-          <Breadcrumbs.Crumb href="/">Home</Breadcrumbs.Crumb>
-          <Breadcrumbs.Crumb href="/sale">Sale</Breadcrumbs.Crumb>
-          <Breadcrumbs.Crumb href="/sale/shoes">
-            Shoes
-          </Breadcrumbs.Crumb>
-        </Breadcrumbs>
+        <BreadCrumbsDisplay />
         <Spacer size={42} />
         <ShoeSidebar />
       </LeftColumn>
     </Wrapper>
   );
 };
+
+const TitleWrapper = styled.div`
+  @media (${QUERIES.tabletAndSmaller}) {
+    display: flex;
+    flex-direction: column;
+  }
+`;
+
+const BreadCrumbsDisplay = () => {
+  return (
+    <Breadcrumbs>
+      <Breadcrumbs.Crumb href="/">Home</Breadcrumbs.Crumb>
+      <Breadcrumbs.Crumb href="/sale">Sale</Breadcrumbs.Crumb>
+      <Breadcrumbs.Crumb href="/sale/shoes">Shoes</Breadcrumbs.Crumb>
+    </Breadcrumbs>
+  );
+};
+
+const BreadcrumbsMobileWrapper = styled.div`
+  display: none;
+  @media (${QUERIES.tabletAndSmaller}) {
+    display: revert;
+  }
+`;
+
+const SelectWrapper = styled.div`
+  @media (${QUERIES.phoneAndSmaller}) {
+    display: none;
+  }
+`;
 
 const Wrapper = styled.div`
   display: flex;
@@ -51,6 +82,10 @@ const Wrapper = styled.div`
 
 const LeftColumn = styled.div`
   flex-basis: 248px;
+
+  @media (${QUERIES.tabletAndSmaller}) {
+    display: none;
+  }
 `;
 
 const MainColumn = styled.div`
@@ -61,6 +96,10 @@ const Header = styled.header`
   display: flex;
   justify-content: space-between;
   align-items: baseline;
+
+  @media (${QUERIES.tabletAndSmaller}) {
+    align-items: flex-end;
+  }
 `;
 
 const Title = styled.h2`
